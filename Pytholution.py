2cbd671e5496be9ac653777ff93ad625bd2437f9
likes linux but uses windows <46166499+j-n-dev@users.noreply.github.com>
2021-03-14 18:00:35 +0100

    print results after simulation

--- Pytholution.py
+++ Pytholution.py
@@ -8,24 +8,27 @@ steps = 100
 birthChance = 100
 deathChance = 10
 replicationChance = 0
+pop = 0
 
-if len(sys.argv) == 5:
+if len(sys.argv) == 6:
     steps = int(sys.argv[1])
     birthChance = int(sys.argv[2])
     deathChance = int(sys.argv[3])
     replicationChance = int(sys.argv[4])
-
-# current population
-pop = 0
+    pop = int(sys.argv[5]);
 
 #used later for generating the graph
 pops = []
 
+lowestPop = 0
+peakPop = 0
 for step in range(0, steps):
+    if pop > peakPop: peakPop = pop
+    if pop < lowestPop: lowestPop = pop
     pops.append(pop) # add the current population to the pops array
     # display info about current simulation
-    print("Step: " + str(step))
-    print("Population:" + str(pop))
+    print("Step #" + str(step + 1) + " of " + str(steps))
+    print("Population: " + str(pop))
 
     # this is where the actual simulation occurs
     if rng.randint(0, 100) < birthChance: pop += 1 # chance of creature appearing out of nowhere
@@ -34,8 +37,14 @@ for step in range(0, steps):
         if rng.randint(0, 100) < deathChance: pop -= 1 # chance of creature dying
         if rng.randint(0, 100) < replicationChance: pop += 1 # chance of creature replicating
 
-# graph generation
+# print results idk
+print("-------------------------------------")
+print("The results are in.")
+print("Avg population: " + str(sum(pops) / len(pops)))
+print("Lowest population: " + str(lowestPop))
+print("Peak population: " + str(peakPop))
 
+# graph generation
 # storing data about the population history
 x = np.arange(0, steps)
 y = np.array(pops)
